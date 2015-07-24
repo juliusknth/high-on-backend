9feb3749605721d58e5f186c101ac1fed7e31dbf
Julius Kinuthia <juliusknth@gmail.com>
2015-07-24 10:51:14 +0300

    more features added
    more features added

--- js/index.js
+++ js/index.js
@@ -806,4 +806,323 @@ $(document).ready(function() {
         });
     });
 
+    $(function () {
+
+        $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=world-population-history.csv&callback=?', function (csv) {
+
+            // Parse the CSV Data
+            /*Highcharts.data({
+                csv: data,
+                switchRowsAndColumns: true,
+                parsed: function () {
+                    console.log(this.columns);
+                }
+            });*/
+
+            // Very simple and case-specific CSV string splitting
+            function CSVtoArray(text) {
+                return text.replace(/^"/, '')
+                    .replace(/",$/, '')
+                    .split('","');
+            };
+
+            csv = csv.split(/\n/);
+
+            var countries = {},
+                mapChart,
+                countryChart,
+                numRegex = /^[0-9\.]+$/,
+                quoteRegex = /\"/g,
+                categories = CSVtoArray(csv[1]).slice(4);
+
+            // Parse the CSV into arrays, one array each country
+            $.each(csv.slice(2), function (j, line) {
+                var row = CSVtoArray(line),
+                    data = row.slice(4);
+
+                $.each(data, function (i, val) {
+                    
+                    val = val.replace(quoteRegex, '');
+                    if (numRegex.test(val)) {
+                        val = parseInt(val);
+                    } else if (!val) {
+                        val = null;
+                    }
+                    data[i] = val;
+                });
+                countries[row[1]] = {
+                    name: row[0],
+                    code3: row[1],
+                    data: data
+                };
+            });
+
+            // For each country, use the latest value for current population
+            var data = [];
+            for (var code3 in countries) {
+                var value = null,
+                    year,
+                    itemData = countries[code3].data,
+                    i = itemData.length;
+
+                while (i--) {
+                    if (typeof itemData[i] === 'number') {
+                        value = itemData[i];
+                        year = categories[i];
+                        break;
+                    }
+                }
+                data.push({
+                    name: countries[code3].name,
+                    code3: code3,
+                    value: value,
+                    year: year
+                });
+            }
+            
+            // Add lower case codes to the data set for inclusion in the tooltip.pointFormat
+            var mapData = Highcharts.geojson(Highcharts.maps['custom/world']);
+            $.each(mapData, function () {
+                this.id = this.properties['hc-key']; // for Chart.get()
+                this.flag = this.id.replace('UK', 'GB').toLowerCase();
+            });
+
+            // Wrap point.select to get to the total selected points
+            Highcharts.wrap(Highcharts.Point.prototype, 'select', function (proceed) {
+
+                proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+
+                var points = mapChart.getSelectedPoints();
+
+                if (points.length) {
+                    if (points.length === 1) {
+                        $('#info #flag').attr('class', 'flag ' + points[0].flag);
+                        $('#info h2').html(points[0].name);
+                    } else {
+                        $('#info #flag').attr('class', 'flag');
+                        $('#info h2').html('Comparing countries');
+
+                    }
+                    $('#info .subheader').html('<h4>Historical population</h4><small><em>Shift + Click on map to compare countries</em></small>')
+
+                    if (!countryChart) {
+                        countryChart = $('#country-chart').highcharts({
+                            chart: {
+                                height: 250,
+                                spacingLeft: 0
+                            },
+                            credits: {
+                                enabled: false
+                            },
+                            title: {
+                                text: null
+                            },
+                            subtitle: {
+                                text: null
+                            },
+                            xAxis: {
+                                tickPixelInterval: 50,
+                                crosshair: true
+                            },
+                            yAxis: {
+                                title: null,
+                                opposite: true
+                            },
+                            tooltip: {
+                                shared: true
+                            },
+                            plotOptions: {
+                                series: {
+                                    animation: {
+                                        duration: 500
+                                    },
+                                    marker: {
+                                        enabled: false
+                                    },
+                                    threshold: 0,
+                                    pointStart: parseInt(categories[0]),
+                                }
+                            }
+                        }).highcharts();
+                    }
+
+                    $.each(points, function (i) {
+                        // Update
+                        if (countryChart.series[i]) {
+                            /*$.each(countries[this.code3].data, function (pointI, value) {
+                                countryChart.series[i].points[pointI].update(value, false);
+                            });*/
+                            countryChart.series[i].update({
+                                name: this.name,
+                                data: countries[this.code3].data,
+                                type: points.length > 1 ? 'line' : 'area'
+                            }, false);
+                        } else {
+                            countryChart.addSeries({
+                                name: this.name,
+                                data: countries[this.code3].data,
+                                type: points.length > 1 ? 'line' : 'area'
+                            }, false);
+                        }
+                    });
+                    while (countryChart.series.length > points.length) {
+                        countryChart.series[countryChart.series.length - 1].remove(false);
+                    }
+                    countryChart.redraw();
+
+                } else {
+                    $('#info #flag').attr('class', '');
+                    $('#info h2').html('');
+                    $('#info .subheader').html('');
+                    if (countryChart) {
+                        countryChart = countryChart.destroy();
+                    }
+                }
+
+                
+
+            });
+            
+            // Initiate the map chart
+            mapChart = $('#container7').highcharts('Map', {
+                
+                title : {
+                    text : 'Population history by country'
+                },
+
+                subtitle: {
+                    text: 'Source: <a href="http://data.worldbank.org/indicator/SP.POP.TOTL/countries/1W?display=default">The World Bank</a>'
+                },
+
+                mapNavigation: {
+                    enabled: true,
+                    buttonOptions: {
+                        verticalAlign: 'bottom'
+                    }
+                },
+
+                colorAxis: {
+                    type: 'logarithmic',
+                    endOnTick: false,
+                    startOnTick: false,
+                    min: 50000
+                },
+
+                tooltip: {
+                    footerFormat: '<span style="font-size: 10px">(Click for details)</span>'
+                },
+
+                series : [{
+                    data : data,
+                    mapData: mapData,
+                    joinBy: ['iso-a3', 'code3'],
+                    name: 'Current population',
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    states: {
+                        select: {
+                            color: '#a4edba',
+                            borderColor: 'black',
+                            dashStyle: 'shortdot'
+                        }
+                    }
+                }]
+            }).highcharts();
+
+            // Pre-select a country
+            mapChart.get('us').select();
+        });
+    });
+
+    $(function () {
+        $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlcv.json&callback=?', function (data) {
+
+            // split the data set into ohlc and volume
+            var ohlc = [],
+                volume = [],
+                dataLength = data.length,
+                // set the allowed units for data grouping
+                groupingUnits = [[
+                    'week',                         // unit name
+                    [1]                             // allowed multiples
+                ], [
+                    'month',
+                    [1, 2, 3, 4, 6]
+                ]],
+
+                i = 0;
+
+            for (i; i < dataLength; i += 1) {
+                ohlc.push([
+                    data[i][0], // the date
+                    data[i][1], // open
+                    data[i][2], // high
+                    data[i][3], // low
+                    data[i][4] // close
+                ]);
+
+                volume.push([
+                    data[i][0], // the date
+                    data[i][5] // the volume
+                ]);
+            }
+
+
+            // create the chart
+            $('#container17').highcharts('StockChart', {
+
+                rangeSelector: {
+                    selected: 1
+                },
+
+                title: {
+                    text: 'AAPL Historical'
+                },
+
+                yAxis: [{
+                    labels: {
+                        align: 'right',
+                        x: -3
+                    },
+                    title: {
+                        text: 'OHLC'
+                    },
+                    height: '60%',
+                    lineWidth: 2
+                }, {
+                    labels: {
+                        align: 'right',
+                        x: -3
+                    },
+                    title: {
+                        text: 'Volume'
+                    },
+                    top: '65%',
+                    height: '35%',
+                    offset: 0,
+                    lineWidth: 2
+                }],
+
+                series: [{
+                    type: 'candlestick',
+                    name: 'AAPL',
+                    data: ohlc,
+                    dataGrouping: {
+                        units: groupingUnits
+                    }
+                }, {
+                    type: 'column',
+                    name: 'Volume',
+                    data: volume,
+                    yAxis: 1,
+                    dataGrouping: {
+                        units: groupingUnits
+                    }
+                }]
+            });
+        });
+    });
+
+    $('#nav').affix({});
+
 });
---
+++ js/user.js
@@ -0,0 +1,9 @@
+$(function () {
+
+  $('#myAffix').affix({
+      offset: {
+        top: $('#jumbotron').height()
+      }
+  });
+
+});

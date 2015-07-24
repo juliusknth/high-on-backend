fc469ea5fdb7b83956c975229d2a4aa612cbb3d2
Julius Kinuthia <juliusknth@gmail.com>
2015-07-24 16:29:32 +0300

    options tab fixed

--- js/index.js
+++ js/index.js
@@ -1123,6 +1123,81 @@ $(document).ready(function() {
         });
     });
 
+    $(function () {
+        var seriesOptions = [],
+            seriesCounter = 0,
+            names = ['MSFT', 'AAPL', 'GOOG'],
+            // create the chart when all data is loaded
+            createChart = function () {
+    
+                $('#container18').highcharts('StockChart', {
+    
+                    rangeSelector: {
+                        selected: 4
+                    },
+    
+                    yAxis: {
+                        labels: {
+                            formatter: function () {
+                                return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                            }
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 2,
+                            color: 'silver'
+                        }]
+                    },
+    
+                    plotOptions: {
+                        series: {
+                            compare: 'percent'
+                        }
+                    },
+    
+                    tooltip: {
+                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                        valueDecimals: 2
+                    },
+    
+                    series: seriesOptions
+                });
+            };
+    
+        $.each(names, function (i, name) {
+    
+            $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+    
+                seriesOptions[i] = {
+                    name: name,
+                    data: data
+                };
+    
+                // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                // we keep a counter and create the chart when all the data is loaded.
+                seriesCounter += 1;
+    
+                if (seriesCounter === names.length) {
+                    createChart();
+                }
+            });
+        });
+    });
+
+    $(function() { 
+        // for bootstrap 3 use 'shown.bs.tab', for bootstrap 2 use 'shown' in the next line
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+            // save the latest tab; use cookies if you like 'em better:
+            localStorage.setItem('lastTab', $(this).attr('href'));
+        });
+
+        // go to the latest tab, if it exists:
+        var lastTab = localStorage.getItem('lastTab');
+        if (lastTab) {
+            $('[href="' + lastTab + '"]').tab('show');
+        }
+    });
+
     $('#nav').affix({});
 
 });
